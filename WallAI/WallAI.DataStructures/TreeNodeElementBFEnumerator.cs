d3c0ab31204a0235c8dafb5e040c8975bf07ba18
gentian kasa <kasa.gentian@hotmail.com>
2016-01-24 23:18:53 +0100

    Re-added due to some strange behavior with the git client

---
+++ WallAI/WallAI.DataStructures/TreeNodeElementBFEnumerator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WallAI.DataStructures
+{
+    public class TreeNodeElementBFEnumerator<TElem> : IEnumerator<TElem>
+    {
+        private TreeNodeBFEnumerator<TElem> _backgroundEnumerator;
+
+        public TElem Current => _backgroundEnumerator.Current.Element;
+
+        object IEnumerator.Current => _backgroundEnumerator.Current.Element;
+
+        public TreeNodeElementBFEnumerator(ITreeNode<TElem> startingPoint)
+            : this(startingPoint, -1)
+        { }
+
+        public TreeNodeElementBFEnumerator(ITreeNode<TElem> startingPoint,
+                                           int depthLimit)
+        {
+            _backgroundEnumerator = new TreeNodeBFEnumerator<TElem>(startingPoint, depthLimit);
+        }
+
+        public void Dispose()
+        {
+            _backgroundEnumerator.Dispose();
+        }
+
+        public bool MoveNext()
+        {
+            return _backgroundEnumerator.MoveNext();
+        }
+
+        public void Reset()
+        {
+            _backgroundEnumerator.Reset();
+        }
+    }
+}
